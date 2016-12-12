18ababa838eb38a80b0685b1c8a3e7206e54939f
manifesti <riku.torenius@gmail.com>
2016-12-12 06:27:49 +0200

    Signed-off-by: manifesti <riku.torenius@gmail.com>

--- data/random.js
+++ data/random.js
@@ -2,10 +2,13 @@
 <!-- The JavaScript Source!! http://www.javascriptsource.com -->
 
 <!-- Begin
-var howMany = -1;  // max number of items listed below
+var howMany = 3;  // max number of items listed below
 var page = new Array(howMany+1);
 
 page[0]="http://toreni.us/1/";
+page[1]="http://toreni.us/2/";
+page[2]="http://toreni.us/3/";
+page[3]="http://toreni.us/4/";
 
 function rndnumber(){
 	var randscript = -1;
