87311ed5f235ebe23c1801c3878aa282fdd09b25
manifesti <riku.torenius@gmail.com>
2016-12-12 06:17:50 +0200

    Signed-off-by: manifesti <riku.torenius@gmail.com>

--- data/random.js
+++ data/random.js
@@ -2,17 +2,17 @@
 <!-- The JavaScript Source!! http://www.javascriptsource.com -->
 
 <!-- Begin
-var howMany = 0;  // max number of items listed below
+var howMany = -1;  // max number of items listed below
 var page = new Array(howMany+1);
 
 page[0]="http://toreni.us/1/index.html";
 
 function rndnumber(){
-var randscript = -1;
-while (randscript < 0 || randscript > howMany || isNaN(randscript)){
-randscript = parseInt(Math.random()*(howMany+1));
-}
-return randscript;
+	var randscript = -1;
+	while (randscript < 0 || randscript > howMany || isNaN(randscript)){
+		randscript = parseInt(Math.random()*(howMany+1));
+	}
+	return randscript;
 }
 quo = rndnumber();
 quox = page[quo];
