13ccbd579abe930dc6485459f399c198fb60d8f2
manifesti <riku.torenius@gmail.com>
2017-03-28 03:27:39 +0300

    Signed-off-by: manifesti <riku.torenius@gmail.com>

--- data/youtube.js
+++ data/youtube.js
@@ -7,7 +7,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 	  
 var index = 0;
 var videos = ["i648EZEQbug", "BHdg_0DFHMw", "9XcpMCly1IY", "DKwbZ4LWT9Q", "TQwvnaf-_Eg", "JsQIqwh7Y8o",
-			  "umCq5V36aT4", "mNJcTqHKdZE", "4_uggscguzs", "de1oJfoDU2c", "C_ysRL4JgQE", "sP4o9veYykg",
+			  "umCq5V36aT4", "mNJcTqHKdZE", "4_uggscguzs", "de1oJfoDU2c", "ln_L6995T4E", "sP4o9veYykg",
 			  "Dbhss_w9Lsc", "z9Ke1aBnQgQ", "odAqNXLipNA", "MOSlaX19Zw8", "Tu14joqyqP4", "LKkGX0T3MdE",
 			  "CSnmyJGNo6U", "W0mQBnvVARk", "FkhpxmF8s-w", "o7eSBwEBwOk", "6c16QdPzS7o", "lyLZmds4QWo",
 			  "QbcdZnYVcjw", "gzN6EMspLAU", "NDbEgRPQHA0", "zF66J4FeLYo", "bBnTrU4XE8M", "OMyiqDl_v_s",
@@ -16,7 +16,7 @@ var videos = ["i648EZEQbug", "BHdg_0DFHMw", "9XcpMCly1IY", "DKwbZ4LWT9Q", "TQwvn
 			  "7EEH8Wj1OqY", "_s3Lfsn5MV4", "P0IrNQsfL9c", "7-g__catIIU", "4BZgpQ_KoOw", "J0cvJ8ScfQw",
 			  "x6ZKufGsVV4", "BiEMkVdOBCY", "U5J23g-uH5I", "2g4dakkzsj4", "hSkES0hh_pY", "CoeQt7ZX5Vc",
 			  "5qQbDGM1fV0", "s6EkPdMit6s", "Cd_31H9qAWU", "iJwhnS3eKco", "Zepq2WOWsqQ", "NWqrHcc6p5M",
-			  "kDgyutf023A", "2b3Purwf4GU", "s32MKzq9e3U", "xbDok-pJLu0", "t-ZAcEbHaeQ", "ln_L6995T4E"];
+			  "kDgyutf023A", "2b3Purwf4GU", "s32MKzq9e3U", "xbDok-pJLu0", "t-ZAcEbHaeQ"];
 randomizeArray(videos);
 
 var videoIndex = videos[index];
