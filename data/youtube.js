734e2499eba59b6ca0917e01e6ec2fddeedb95b6
manifest <riku.torenius@gmail.com>
2017-11-08 22:44:44 +0200

    create comments for JS and improve html for returning as a school project

--- data/youtube.js
+++ data/youtube.js
@@ -1,11 +1,13 @@
-//	This code loads the IFrame Player API code asynchronously.
+// load youtube Iframe API
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-	  
+// player starts from index 0 in videos
 var index = 0;
+
+// given youtube video IDs
 var videos = ["i648EZEQbug", "BHdg_0DFHMw", "9XcpMCly1IY", "DKwbZ4LWT9Q", "TQwvnaf-_Eg", "JsQIqwh7Y8o",
 			  "umCq5V36aT4", "mNJcTqHKdZE", "4_uggscguzs", "de1oJfoDU2c", "ln_L6995T4E", "sP4o9veYykg",
 			  "Dbhss_w9Lsc", "z9Ke1aBnQgQ", "odAqNXLipNA", "MOSlaX19Zw8", "Tu14joqyqP4", "LKkGX0T3MdE",
@@ -17,10 +19,11 @@ var videos = ["i648EZEQbug", "BHdg_0DFHMw", "9XcpMCly1IY", "DKwbZ4LWT9Q", "TQwvn
 			  "x6ZKufGsVV4", "BiEMkVdOBCY", "U5J23g-uH5I", "2g4dakkzsj4", "hSkES0hh_pY", "CoeQt7ZX5Vc",
 			  "5qQbDGM1fV0", "s6EkPdMit6s", "Cd_31H9qAWU", "iJwhnS3eKco", "Zepq2WOWsqQ", "NWqrHcc6p5M",
 			  "kDgyutf023A", "mXhZrU5w1Wo", "s32MKzq9e3U", "xbDok-pJLu0", "5pILkGqOCWs", "Rvz5GxqLAMk"];
+// shuffle the list on page load
 randomizeArray(videos);
-
+// initial video id to give to player
 var videoIndex = videos[index];
-	  
+// initialize player in HTML-div "player"
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
 		height: '94%',
@@ -33,18 +36,19 @@ function onYouTubeIframeAPIReady() {
 		}
 	});
 }
+// as soon as player is ready, play video
 function onPlayerReady(event) {
 	event.target.playVideo();
 }
+// if video ends, play next video from array
 function onPlayerStateChange(event) {
 	playPause(event.data);
 	if (event.data == YT.PlayerState.ENDED) {
 		nextVideo();
 	}
 }
-function newVideoUrl() {
-	return videos[Math.floor(Math.random() * videos.length)];
-}
+
+// function for randomizing the array
 function randomizeArray(array) {
 	for (var i = array.length - 1; i > 0; i--) {
 		var j = Math.floor(Math.random() * (i + 1));
@@ -54,6 +58,7 @@ function randomizeArray(array) {
 	}
     return array;
 }
+// return next index from array until loop back to first
 function nextIndex() {
 	index++;
 	if (index > videos.length - 1) {
@@ -61,6 +66,7 @@ function nextIndex() {
 	}
 	return index;
 }
+// return last index from array until loop back to last
 function lastIndex() {
 	index--;
 	if (index < 0) {
@@ -68,6 +74,7 @@ function lastIndex() {
 	}
 	return index;
 }
+// buttons that play/pause video and go to next/last video in array
 function playPause(playerStatus) {
 	var asema = document.getElementById("playnappula");
 	if (playerStatus == 1) {
@@ -81,9 +88,11 @@ function playPause(playerStatus) {
 		asema.onclick = function(){player.playVideo();};
 	}
 }
+// function for making player load the next video in array
 function nextVideo() {
 	player.loadVideoById(videos[nextIndex()]);
 }
+// function for making player load the last video in array
 function lastVideo() {
 	player.loadVideoById(videos[lastIndex()]);
 }
